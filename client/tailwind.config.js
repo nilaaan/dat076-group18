08763b729646f8ac8585c4a3951cf7bc21d0e98a
nilaaan <k.andreas.nilsson@hotmail.com>
2025-02-18 11:34:31 +0100

    Added next.skeleton.dev

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,20 @@
+import { skeleton, contentPath } from "@skeletonlabs/skeleton/plugin";
+import * as themes from "@skeletonlabs/skeleton/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    contentPath(import.meta.url, "react"),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    skeleton({
+      // NOTE: each theme included will increase the size of your CSS bundle
+      themes: [themes.cerberus, themes.rose],
+    }),
+  ],
 }
