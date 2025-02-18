cde4f1153b12d6fb14a38452cd599fc91bcab43a
HugoUnosson <hugo.unosson@gmail.com>
2025-02-18 14:06:07 +0100

    Small change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,19 +7,21 @@ import { CircleUser } from 'lucide-react';
 function App() {
   return (
     <Router>
-      <nav className="flex h-16 w-full bg-white text-black text-3xl justify-between items-center px-4 font-bold border-b-2 border-blue-500">
-        <div className="space-x-4">
+      <nav className="flex h-16 w-full bg-white text-black text-xl justify-between items-center px-4 font-bold border-b-2 border-blue-500">
+        <div className="flex space-x-4">
+          <h1>Fantasy League</h1>
           <Link to="/team" className="hover:text-blue-500">Team</Link>
           <Link to="/player" className="hover:text-blue-500">Player</Link>
         </div>
-        <h1>Fantasy League</h1>
-        <Link to="/login"><CircleUser className="hover:text-blue-500" size={40}/></Link>
+        <Link to="/login"><CircleUser className="hover:text-blue-500" size={32} /></Link>
       </nav>
 
-      <Routes>
-        <Route path="/team" element={<TeamView />}></Route>
-        <Route path="/player" element={<PlayerView />}></Route>
-      </Routes>
+      <main className="px-4">
+        <Routes>
+            <Route path="/team" element={<TeamView />}></Route>
+            <Route path="/player" element={<PlayerView />}></Route>
+        </Routes>
+      </main>
     </Router>
   )
 }
