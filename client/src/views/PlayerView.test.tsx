e623b4d5b58511ac4900518d5c76cd85aa60911e
ludwigal <alexandersson_ludwig@yahoo.se>
2025-02-18 20:25:31 +0100

    PlayerView tests seem to work.

---
+++ client/src/views/PlayerView.test.tsx
@@ -0,0 +1,51 @@
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import axios, { AxiosError } from 'axios';
+import PlayerView from './PlayerView';
+
+jest.mock('axios');
+
+const mockAxios = axios.get as jest.Mock;
+
+test('Displays ChoiceBox initially', async () => {
+    const { getByText} = render(<PlayerView />);
+
+    await waitFor(() => {
+        expect(getByText(/Select a player/i)).toBeInTheDocument();
+    });
+});
+
+test('Displays error message if player data cannot be loaded', async () => {
+    mockAxios.mockRejectedValueOnce(new AxiosError());
+    
+    const { getByRole, getByText } = render(<PlayerView />);
+
+    fireEvent.change(getByRole('combobox'), { target: { value: '1' } });
+
+    await waitFor(() => {
+        expect(getByText(/Error loading player/i)).toBeInTheDocument();
+    });
+});
+
+test('Displays player information when data is successfully loaded', async () => {
+    const mockPlayer = {
+        id: '1',
+        name: 'Mock Player',
+        price: 10,
+        position: 'Forward',
+        number: 9,
+        club: 'Mock Club',
+        points: 20,
+    };
+
+    mockAxios.mockResolvedValueOnce({ data: mockPlayer });
+
+    const { getByRole, getByText } = render(<PlayerView />);
+
+    fireEvent.change(getByRole('combobox'), { target: { value: '1' } });
+
+    await waitFor(() => {
+        expect(getByText(mockPlayer.name)).toBeInTheDocument();
+        expect(getByText(/Mock Club/i)).toBeInTheDocument();
+
+    });
+});
