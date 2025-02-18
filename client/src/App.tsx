c4b9d552b4be0202a4e3989759e253badc90c3c7
HugoUnosson <hugo.unosson@gmail.com>
2025-02-18 12:16:18 +0100

    Home page styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TeamView from './views/TeamView.tsx';
 import PlayerView from './views/PlayerView.tsx';
 import './App.css'
+import { CircleUser } from 'lucide-react';
 
 function App() {
   return (
     <Router>
-      <nav className="">
-        <Link to="/team" className="">Team</Link>
-        <Link to="/player" className="">Player</Link>
+      <nav className="flex h-16 w-full bg-white text-black text-3xl justify-between items-center px-4 font-bold border-b-2 border-blue-500">
+        <div className="space-x-4">
+          <Link to="/team" className="hover:text-blue-500">Team</Link>
+          <Link to="/player" className="hover:text-blue-500">Player</Link>
+        </div>
+        <h1>Fantasy League</h1>
+        <Link to="/login"><CircleUser className="hover:text-blue-500" size={40}/></Link>
       </nav>
 
       <Routes>
