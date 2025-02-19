eeb4fcc4e166f65646030cfc95fecf2443984ba9
nilaaan <k.andreas.nilsson@hotmail.com>
2025-02-19 14:47:31 +0100

    Playerlist start

--- client/src/components/PlayerList.tsx
+++ client/src/components/PlayerList.tsx
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 import { Player } from '../Types.ts';
-import PlayerListItem from './PlayerListItem.tsx';
+import { Accordion } from '@skeletonlabs/skeleton-react';
+import { User } from 'lucide-react';
 
 interface PlayerListProps {
     players: Player[];
 }
 
 function PlayerList({ players = [] }: PlayerListProps) {
+    const [value, setValue] = useState<string[]>([]);
+
     return (
-        <div className="flex flex-col">
+        <Accordion value={value} onValueChange={setValue} multiple>
             {players.map((player) => (
-                <PlayerListItem key={player.id} player={player}></PlayerListItem>
+                <div key={player.id}>
+                    <hr className="hr"></hr>
+                    <Accordion.Item value={`${player.id}`}>
+                        <Accordion.Control>
+                            <div className="flex flex-row gap-2 p-2">
+                                <User></User>
+                                <p>{player.name}</p>
+                            </div>
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <th className="opacity-50">Position:</th>
+                                        <td>{player.position}</td>
+                                    </tr>
+                                    <tr>
+                                        <th className="opacity-50">#</th>
+                                        <td>{player.number}</td>
+                                    </tr>
+                                    <tr>
+                                        <th className="opacity-50">Club</th>
+                                        <td>{player.club}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+                </div>
             ))}
-        </div>
+        </Accordion>
     )
 }
 
--- client/src/components/PlayerSlot.tsx
+++ client/src/components/PlayerSlot.tsx
@@ -1,20 +1,26 @@
 import { Player } from '../Types.ts';
-import { Plus } from 'lucide-react';
+import { Plus, User } from 'lucide-react';
 
 interface PlayerSlotProps {
     player?: Player;
 }
 
 function PlayerSlot({ player }: PlayerSlotProps) {
-
     return (
         <div className="card card-hover preset-filled-surface-100-900 opacity-75 w-[20%] border-4 border-dashed border-surface-300-700 flex flex-col p-2">
             <header className="aspect-square">
-                <Plus className="w-[50%] h-full m-auto opacity-50"></Plus>
+                {player ?
+                    (
+                        <User className="w-[50%] h-full m-auto opacity-50" />
+                    ) :
+                    (
+                        <Plus className="w-[50%] h-full m-auto opacity-50" />
+                    )
+                }
             </header>
             <hr className="hr border-t-2 border-surface-300-700"></hr>
             <footer>
-                <p className="text-center py-2">Test{player ? player.name : <>&nbsp;</>}</p>
+                <p className="text-center py-2">{player ? player.name : <>&nbsp;</>}</p>
             </footer>
         </div>
     )
