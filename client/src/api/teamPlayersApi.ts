7074a61a15e1c86e3a552e3e9e663801bc9a7267
HugoUnosson <hugo.unosson@gmail.com>
2025-02-20 11:34:43 +0100

    Login setup

--- client/src/api/teamPlayersApi.ts
+++ client/src/api/teamPlayersApi.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { Player } from '../Types.ts';
 
+axios.defaults.withCredentials = true;
+
 export const getTeamPlayers = async(): Promise<Player[]> => {
     const res = await axios.get<Player[]>('http://localhost:8080/team/players');
     return res.data;
--- server/src/start.ts
+++ server/src/start.ts
@@ -2,10 +2,27 @@ import express from "express";
 import { playerRouter } from "./router/player";
 import { teamRouter } from "./router/team";
 import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
 
-export const app = express(); 
+export const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+dotenv.config();
+if (!process.env.SESSION_SECRET) {
+    console.log("Could not find SESSION_SECRET in .env file");
+    process.exit();
+}
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
 app.use("/player", playerRouter);
 app.use("/team", teamRouter);
