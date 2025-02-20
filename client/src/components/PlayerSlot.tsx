985e14d68c9c8f1352d5a79b647288c9d16c71c9
nilaaan <k.andreas.nilsson@hotmail.com>
2025-02-20 11:49:02 +0100

    Made players removable from slots

--- client/src/components/PlayerSlot.tsx
+++ client/src/components/PlayerSlot.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Player } from '../Types.ts';
-import { User } from 'lucide-react';
+import { User, X } from 'lucide-react';
 
 interface PlayerSlotProps {
     initialPlayer?: Player;
@@ -37,15 +37,24 @@ function PlayerSlot({ initialPlayer, setPlayerAvailable }: PlayerSlotProps) {
         }
     }
 
+    function removePlayer() {
+        if (player) {
+            console.log("removed");
+            setPlayerAvailable(player.id, true);
+            setPlayer(undefined);
+        }
+    }
+
     return (
-        <div 
-            className={`card preset-filled-surface-100-900 opacity-75 w-[20%] border-4 border-surface-300-700 flex flex-col p-2
+        <div
+            className={`card preset-filled-surface-100-900 opacity-75 w-[20%] border-4 border-surface-300-700 flex flex-col
                 ${player ? "cursor-grab card-hover" : "border-dashed cursor-not-allowed"}`}
             onDrop={drop}
             onDragOver={dragOver}
         >
-            <header className="aspect-square">
+            <header className="aspect-square relative">
                 <User className="w-[50%] h-full m-auto opacity-50" />
+                <button type="button" className="btn absolute top-0 right-0 p-0 px-1 m-0 rounded-none rounded-bl-lg preset-filled-surface-300-700" onMouseDown={removePlayer}><X></X></button>
             </header>
             <hr className="hr border-t-2 border-surface-300-700"></hr>
             <footer>
