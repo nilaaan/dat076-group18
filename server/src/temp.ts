53f6f507e4577883d1d2c1d0d482770f54aebe95
HugoUnosson <hugo.unosson@gmail.com>
2025-02-22 01:01:33 +0100

    deleted file

--- server/src/temp.ts
+++
@@ -1,8 +0,0 @@
-import { registerUser } from "./service/auth"; 
-
-const testRegister = async () => {
-    const result = await registerUser("testUser", "securePassword");
-    console.log(result);
-};
-
-testRegister();
