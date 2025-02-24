5d06e88047185b559ab9facad9d0398dc997fe3b
nilaaan <k.andreas.nilsson@hotmail.com>
2025-02-24 21:45:20 +0100

    Fixed app not taking whole width on small screens. Added github link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import SellView from './views/SellView.tsx';
 import { AppBar } from '@skeletonlabs/skeleton-react';
 
 import './App.css'
-import { CircleUser } from 'lucide-react';
+import { CircleUser, Github, Circle } from 'lucide-react';
 import BalanceView from './views/BalanceView.tsx';
 
 function NavLink({ to, children }: { to: string, children: React.ReactNode }) {
@@ -28,36 +28,41 @@ function App() {
   return (
     <Router>
       <AppBar classes="!p-0">
-        <AppBar.Toolbar classes="flex justify-between">
+        <AppBar.Toolbar classes="flex justify-between h-16 md:h-auto">
           <AppBar.ToolbarLead>
-            <Link to="/" className="py-4 pl-10">
-              <h1 className="h4 hover:text-primary-400-600">Fantasy League</h1>
-            </Link> 
+            <Link to="/" className="py-4 pl-10 hidden md:block">
+              <h1 className="h4 hover:text-primary-400-600 whitespace-nowrap">Fantasy League</h1>
+            </Link>
           </AppBar.ToolbarLead>
           <AppBar.ToolbarCenter classes="!grow-0">
             <div className="flex gap-4 h-full">
               <NavLink to="/team">Team</NavLink>
               <NavLink to="/player">Player</NavLink>
               <NavLink to="/balance">Balance</NavLink>
-              <NavLink to= "/buy">Buy</NavLink>
-              <NavLink to= "/sell">Sell</NavLink>
+              <NavLink to="/buy">Buy</NavLink>
+              <NavLink to="/sell">Sell</NavLink>
             </div>
           </AppBar.ToolbarCenter>
-          <AppBar.ToolbarTrail classes="flex items-center px-10">
-            <Link to="/login">
-              <CircleUser className="hover:text-primary-400-600" size={32} />
+          <AppBar.ToolbarTrail classes="flex items-center pl-20 pr-10">
+            <Link to="/login" className="hover:text-primary-400-600">
+              <CircleUser size={32} />
+            </Link>
+            <Link to="https://github.com/nilaaan/dat076-group18" className="hover:text-primary-400-600">
+                <Github size={32} className="hover:text-primary-400-600" />
             </Link>
           </AppBar.ToolbarTrail>
         </AppBar.Toolbar>
       </AppBar>
-      <Routes>
-        <Route path="/" element={<div />}></Route>
-        <Route path="/team" element={<TeamView />}></Route>
-        <Route path="/player" element={<PlayerView />}></Route>
-        <Route path="/balance" element={<BalanceView />}></Route>
-        <Route path="/buy" element={<BuyView />}></Route>
-        <Route path="/sell" element={<SellView />}></Route>
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<div />}></Route>
+          <Route path="/team" element={<TeamView />}></Route>
+          <Route path="/player" element={<PlayerView />}></Route>
+          <Route path="/balance" element={<BalanceView />}></Route>
+          <Route path="/buy" element={<BuyView />}></Route>
+          <Route path="/sell" element={<SellView />}></Route>
+        </Routes>
+      </main>
     </Router>
   )
 }
