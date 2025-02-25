d65344c5cd84810464c60eaef8a7515f40154b28
nilaaan <k.andreas.nilsson@hotmail.com>
2025-02-25 01:41:09 +0100

    Added vertical divider to separate login button from theme toggle and github link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -45,10 +45,11 @@ function App() {
             </div>
           </AppBar.ToolbarCenter>
           <AppBar.ToolbarTrail classes="flex items-center pl-20 pr-10">
-            <ThemeToggle />
             <Link to="/login" className="hover:text-primary-400-600">
               <CircleUser size={28} />
             </Link>
+            <span className="vr border-l-2 border-surface-900-100 h-2/3"></span>
+            <ThemeToggle />
             <Link to="https://github.com/nilaaan/dat076-group18" className="hover:text-primary-400-600">
                 <Github size={28} className="hover:text-primary-400-600" />
             </Link>
