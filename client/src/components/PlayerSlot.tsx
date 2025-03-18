509044a42756a8dec94cc8d08942922ad61bb77a
nilaaan <k.andreas.nilsson@hotmail.com>
2025-03-18 14:05:41 +0100

    Added comments to playerslot and fieldview

--- client/src/components/PlayerSlot.tsx
+++ client/src/components/PlayerSlot.tsx
@@ -10,20 +10,27 @@ interface PlayerSlotProps {
 }
 
 function PlayerSlot({ initialPlayer, onInfoClick }: PlayerSlotProps) {
+    // State for the player data
     const [player, setPlayer] = useState<Player | undefined>();
+    // State for player's last match rating
     const [lastMatchRating, setLastMatchRating] = useState<number | null>(null);
+    // State to track if game session is active
     const [gameSessionActive, setGameSessionActive] = useState<boolean>(false);
+    // State for the current round number
     const [currentRound, setCurrentRound] = useState<number>(0);
 
+    // Update the player state when player changes
     useEffect(() => {
         if (initialPlayer) {
             setPlayer(initialPlayer);
         }
     }, [initialPlayer]);
 
+    // Fetch game session and player rating data when the player changes
     useEffect(() => {
         const fetchGameSessionData = async () => {
             try {
+                // Check if game is active
                 const gameSessionStatus = await isGameSession();
                 if (typeof gameSessionStatus === 'string') {
                     console.error('Error checking game session:', gameSessionStatus);
@@ -31,6 +38,7 @@ function PlayerSlot({ initialPlayer, onInfoClick }: PlayerSlotProps) {
                 }
                 setGameSessionActive(gameSessionStatus);
 
+                // If game session is active, get data for the current round
                 if (gameSessionStatus) {
                     const round = await getCurrentRound();
                     if (typeof round === 'string') {
@@ -39,6 +47,7 @@ function PlayerSlot({ initialPlayer, onInfoClick }: PlayerSlotProps) {
                     }
                     setCurrentRound(round);
 
+                    // If player is set and there is a previous round, fetch the player's rating in the previous round
                     if (round >= 2 && player) {
                         const rating = await getRating(player.id, round - 1);
                         if (typeof rating === 'string') {
@@ -56,32 +65,38 @@ function PlayerSlot({ initialPlayer, onInfoClick }: PlayerSlotProps) {
         fetchGameSessionData();
     }, [player]);
 
+    // Content to render inside this slot if the player is set
     const content = (
         <>
             <header className="aspect-square relative">
+                {/*Render player details if player data is set*/}
                 {player ?
-                    (<>
-                        <img src={player.image} className="w-full h-full"></img>
-                        {/* <a href={`/player/${player.id}`}> */}
+                    (
+                        <>
+                            <img src={player.image} className="w-full h-full"></img>
                             <button type="button" className="btn absolute rounded-none top-0 left-0 p-2 preset-filled-surface-400-600" onClick={onInfoClick}>
                                 <Info />
                             </button>
-                        {/* </a> */}
-                        {gameSessionActive && currentRound >= 2 && player && (
-                            <p className={`
-                                absolute top-0 right-0 p-2 preset-filled-surface-400-600 font-bold
-                                ${
-                                    lastMatchRating === null ? "!text-white" :
-                                    lastMatchRating <= 3 ? "!text-error-500" :
-                                    lastMatchRating <= 6 ? "!text-warning-500" :
-                                    lastMatchRating <= 8 ? "!text-success-500" :
-                                    "!text-green-600"
-                                }`}>
-                                {lastMatchRating ? lastMatchRating.toFixed(1) : "N/A"}
-                            </p>
-                        )}
-                    </>) :
-                    (<Plus className="w-3/4 h-full m-auto" />)
+                            {/*Display the player's rating in the last round if there is one*/}
+                            {gameSessionActive && currentRound >= 2 && player && (
+                                <p className={`
+                                    absolute top-0 right-0 p-2 preset-filled-surface-400-600 font-bold
+                                    ${
+                                        lastMatchRating === null ? "!text-white" : // White for N/A
+                                        lastMatchRating <= 3 ? "!text-error-500" : // Red for bad rating
+                                        lastMatchRating <= 6 ? "!text-warning-500" : // Yellow for ok rating
+                                        lastMatchRating <= 8 ? "!text-success-500" : // Light green for good rating
+                                        "!text-green-600" // Dark green for great rating
+                                    }`}>
+                                    {/*Display rating or N/A if not available*/}
+                                    {lastMatchRating ? lastMatchRating.toFixed(1) : "N/A"}
+                                </p>
+                            )}
+                        </>
+                    ) : (
+                        // Display a plus to indicate the user can add a player to this empty slot
+                        <Plus className="w-3/4 h-full m-auto" />
+                    )
                 }
             </header>
             <hr className="hr border-t-2 border-surface-400-600"></hr>
@@ -90,14 +105,15 @@ function PlayerSlot({ initialPlayer, onInfoClick }: PlayerSlotProps) {
             </footer>
         </>
     )
-
     return (
         <div
             className="card card-hover preset-filled-surface-200-800 cursor-pointer w-[20%] sm:w-[15%] md:w-[10%] border-2 border-surface-400-600 flex flex-col opacity-80"
         >
-            { player ? (
+            {player ? 
+            (
                 content
             ) : (
+                // If no player data is available, render the playerslot with a link around it to buy view
                 <a href="/player">{content}</a>
             )}
         </div>
